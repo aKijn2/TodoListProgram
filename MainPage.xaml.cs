e137563e6f2eb42e94adb8b03a19e0698a8255ff
aKijn2 <mrkinder20041010@gmail.com>
2026-01-23 20:23:06 +0100

    small changes, added rotation to change theme button and added fade in to task item cards.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -29,5 +29,21 @@ namespace TaskFlow
                 await _viewModel.InvalidateCacheAsync();
             }
         }
+
+        private async void OnThemeToggleTapped(object sender, EventArgs e)
+        {
+            // 360-degree rotation animation for "premium" feel
+            await ThemeIconLabel.RotateTo(360, 500, Easing.CubicOut);
+            ThemeIconLabel.Rotation = 0;
+        }
+
+        private async void OnTaskItemLoaded(object sender, EventArgs e)
+        {
+            if (sender is View view)
+            {
+                // Fade in animation
+                await view.FadeTo(1, 400, Easing.CubicOut);
+            }
+        }
     }
 }
